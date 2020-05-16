3f72ab71cd1efe16aadf1aa3d385a860a90384a8
Allison Usher <allyjay317@gmail.com>
2020-05-15 18:22:44 -0700

    completed header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,21 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  let header = document.createElement("div")
+   header.classList.add("header")
+  let headerDate = document.createElement("span")
+   headerDate.classList.add("date")
+   headerDate.textContent = "SMARCH 28, 2019"
+  let headerTitle = document.createElement("h1")
+  headerTitle.textContent = "Lambda Times"
+  let headerTemp = document.createElement("span")
+   headerTemp.classList.add("temp")
+   headerTemp.textContent = "98°"
+  
+    header.append(headerDate, headerTitle, headerTemp)
+
+    return header
+}
+
+document.querySelector(".header-container").append(Header())
