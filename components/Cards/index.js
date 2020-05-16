71ebdb860b2bb780983fef7fb2b1514f6a33bb70
Allison Usher <allyjay317@gmail.com>
2020-05-15 18:31:52 -0700

    blocked out component creator for cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,27 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+let cardCreator = function(cardData){
+    let card = document.createElement("div") 
+    card.classList.add("card")
+    let cardHeadline = document.createElement("div") 
+    cardHeadline.classList.add("headline")
+    cardHeadline.textContent = cardData.headline;
+    let cardAuthor = document.createElement("div") 
+    cardAuthor.classList.add("author")
+    let cardImgContainer = document.createElement("div") 
+    cardImgContainer.classList.add("img-container")
+    let cardImg = document.createElement("img") 
+    cardImg.src= cardData.authorPhoto
+    
+    let cardByline = document.createElement("span")
+    cardByline.textContent = `By ${cardData.authorName}`
+
+    card.append(cardHeadline, cardAuthor)
+    cardAuthor.append(cardImgContainer, cardByline)
+    cardImgContainer.append(cardImg)
+
+    return card;
+
+}
