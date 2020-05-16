cfc9121239adadd36fea16ed7000163139add8a2
Allison Usher <allyjay317@gmail.com>
2020-05-15 19:12:17 -0700

    completed basic infinite carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,49 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let carouselCreator = function(){
+  let index = 0
+  let carousel = document.createElement("div") 
+  carousel.classList.add("carousel")
+  let carouselLBtn = document.createElement("div") 
+  carouselLBtn.classList.add("left-button")
+  let carouselImgs = [
+    carouselImg("./assets/carousel/mountains.jpeg"),
+    carouselImg("./assets/carousel/computer.jpeg"),
+    carouselImg("./assets/carousel/trees.jpeg"),
+    carouselImg("./assets/carousel/turntable.jpeg")
+  ]
+  carouselImgs[0].classList.add("imgShow")
+  let carouselRBtn = document.createElement("div") 
+  carouselRBtn.classList.add("right-button")
+
+  carousel.append(carouselLBtn)
+  carouselImgs.forEach(img => carousel.append(img))
+  carousel.append(carouselRBtn)
+
+  carouselLBtn.addEventListener("click", (event)=>{
+    let oldIndex = index
+    index = index == 0 ? carouselImgs.length-1 : --index;
+    carouselImgs[oldIndex].classList.remove("imgShow")
+    carouselImgs[index].classList.add("imgShow")
+  })
+  carouselRBtn.addEventListener("click", (event) =>{
+    let oldIndex = index
+    index = index == carouselImgs.length-1 ? 0 : ++index;
+    carouselImgs[oldIndex].classList.remove("imgShow")
+    carouselImgs[index].classList.add("imgShow")
+  })
+
+  return carousel
+}
+
+let carouselImg = function(source){
+  let img = document.createElement("img")
+  img.src = source
+
+  return img
+}
+
+document.querySelector(".carousel-container").append(carouselCreator())
+
